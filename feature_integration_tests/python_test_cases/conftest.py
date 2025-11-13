292a2381a4546f80bf7dbe445a610ff68e595b5b
Piotr Korkus <piotr.korkus.ext@qorix.ai>
2025-11-13 16:30:30 +0100

    add test attributes (#36)

--- feature_integration_tests/python_test_cases/conftest.py
+++ feature_integration_tests/python_test_cases/conftest.py
@@ -64,3 +64,20 @@ def pytest_sessionstart(session):
 
     except Exception as e:
         pytest.exit(str(e), returncode=1)
+
+
+def pytest_collection_modifyitems(items: list[pytest.Function]):
+    markers_to_process = (
+        "PartiallyVerifies",
+        "FullyVerifies",
+        "Description",
+        "TestType",
+        "DerivationTechnique",
+    )
+    for item in items:
+        # Add custom markers info to XML report
+        for marker in item.iter_markers():
+            if marker.name not in markers_to_process:
+                continue
+
+            item.user_properties.append((marker.name, marker.args[0]))
