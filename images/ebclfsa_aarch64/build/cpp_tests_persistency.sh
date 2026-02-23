38e0028777a2eccd6c52465b38228baac99e59d6
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2026-02-23 16:29:25 +0100

    cleanup

--- images/ebclfsa_aarch64/build/cpp_tests_persistency.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Script to run C++ persistency tests inside the QEMU environment
-# Wrapper due to quoting issues when calling directly from Bazel
-cpp_tests_persistency --name basic.basic --input '{"kvs_parameters":{"instance_id":0}}'
