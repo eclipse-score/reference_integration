c62366fc3d77c863ac0da9acbebd48e1bc54735b
Oliver Pajonk <oliver.pajonk@elektrobit.com>
2026-03-18 17:35:30 +0100

    extract EBcLfSA specific args from QEMU starter and make it generic

--- runners/qemu_aarch64/scripts/run_qemu.sh
+++ runners/qemu_aarch64/scripts/run_qemu.sh
@@ -27,25 +27,29 @@
 
 set -euox pipefail
 
-#BASEFOLDER=$1
+if [[ $# -lt 2 ]]; then
+    echo "Error: This script requires at least 2 parameters"
+    echo "Usage: $0 <kernel> <image> [additional qemu args]"
+    exit 1
+fi
 
-echo "Image: ${2}"
-echo "Kernel: ${1}"
-
-IMAGE="$(rlocation _main/$2)"
 KERNEL="$(rlocation _main/$1)"
+IMAGE="$(rlocation _main/$2)"
 
-echo "Image: ${IMAGE}"
 echo "Kernel: ${KERNEL}"
+echo "Image: ${IMAGE}"
+shift 2
+QEMU_ARGS=("$@")
 
-# Make the image file writable, as QEMU needs to write to it.
+# Make the image file writable, as QEMU needs to write to it
 find -L "$(dirname "${IMAGE}")" -samefile "${IMAGE}" -exec chmod +w {} \;
 
+# default QEMU parameters for running an aarch64 image with virtio devices and user networking
+# these can be overridden by passing additional parameters to the script, e.g. to enable a graphical display or to use a different machine type
 MACHINE="virt,virtualization=true,gic-version=3"
 CPU="cortex-a53"
 SMP="8"
 MEM="4G"
-KERNEL_ARGS=("-append" "root=/dev/vda1 sdk_enable lisa_syscall_whitelist=2026 rw sharedmem.enable_sharedmem=0 init=/usr/bin/ebclfsa-cflinit")
 DISK_ARGS="-device virtio-blk-device,drive=vd0 -drive if=none,format=raw,file=${IMAGE},id=vd0"
 NETWORK_ARGS="-netdev user,id=net0,net=192.168.7.0/24,dhcpstart=192.168.7.2,dns=192.168.7.3,host=192.168.7.5,hostfwd=tcp::2222-:22,hostfwd=tcp::3333-:3333 -device virtio-net-device,netdev=net0 "
 
@@ -62,10 +66,10 @@ qemu-system-aarch64  \
     -cpu "${CPU}" \
     -smp "${SMP}" \
     -kernel "${KERNEL}" \
-    "${KERNEL_ARGS[@]}" \
     ${DISK_ARGS} \
     ${NETWORK_ARGS} \
     -nographic \
     -chardev stdio,id=char0,signal=on,mux=on \
     -mon chardev=char0,mode=readline \
-    -serial chardev:char0
+    -serial chardev:char0 \
+    "${QEMU_ARGS[@]}"
