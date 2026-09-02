555671f5d9bb11936e128c4bdf0aebeffffa0f66
Saumya-R <saumya.rai@qorix.ai>
2026-09-02 16:51:59 +0530

    adding test cases for persistency+lifecycle

---
+++ feature_integration_tests/test_scenarios/rust/src/scenarios/lifecycle/mod.rs
@@ -0,0 +1,25 @@
+// *******************************************************************************
+// Copyright (c) 2026 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// SPDX-License-Identifier: Apache-2.0
+// *******************************************************************************
+
+mod conditional_launching;
+
+use conditional_launching::ConditionalLaunching;
+use test_scenarios_rust::scenario::{ScenarioGroup, ScenarioGroupImpl};
+
+pub fn lifecycle_group() -> Box<dyn ScenarioGroup> {
+    Box::new(ScenarioGroupImpl::new(
+        "lifecycle",
+        vec![Box::new(ConditionalLaunching)],
+        vec![],
+    ))
+}
